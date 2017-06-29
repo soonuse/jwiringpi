0bf46e3d6460f2d44ef2f61e502ff0660ba034d2
soonuse <soonuse@163.com>
2017-06-29 17:20:03 +0800

    Update SH1106SPIDemo.java

--- demo/raspberrypi/sh1106spi/SH1106SPIDemo.java
+++ demo/raspberrypi/sh1106spi/SH1106SPIDemo.java
@@ -1,3 +1,28 @@
+/**
+  * @filename       : SH1106SPIDemo.java
+  * @date           : June 29 2017
+  * @author         : soonuse from Github
+  * @description:
+  *   This demo is written in Java and tested with a Waveshare SH1106 
+  *   1.3inch OLED module working on Raspberry Pi 3 Model B. Before running 
+  *   this demo, you have to enable the SPI interface on the Raspberry Pi. 
+  *   See sudo raspi-config
+  *
+  *   Expected result:
+  *   The OLED displays strings and images.
+  ***********************************************************************
+  * This is a demo of SH1106 1.3inch OLED module working on Raspberry Pi.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU Lesser General Public License for more details.
+  *
+  * You should have received a copy of the GNU Lesser General Public License
+  * along with wiringPi.  If not, see <http://www.gnu.org/licenses/>.
+  ***********************************************************************
+ */
+
 import java.io.*;
 
 public class SH1106SPIDemo {
