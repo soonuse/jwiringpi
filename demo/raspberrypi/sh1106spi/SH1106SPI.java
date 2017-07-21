65e988f766e9355e6e803840aee7a43c3a570263
soonuse <soonuse@163.com>
2017-07-21 10:24:47 +0800

    Update SH1106SPI.java

--- demo/raspberrypi/sh1106spi/SH1106SPI.java
+++ demo/raspberrypi/sh1106spi/SH1106SPI.java
@@ -56,7 +56,7 @@ public class SH1106SPI extends JWiringPiController {
         }
     }
 
-    public void showString(String string, int x, int y, int fontSize, Font font, boolean color) {
+    public void showString(String string, int x, int y, Font font, boolean color) {
         BufferedImage image = new BufferedImage(OLED_WIDTH, OLED_HEIGHT, BufferedImage.TYPE_INT_RGB);
         Graphics2D g2d = image.createGraphics();
         g2d.setFont(font);
@@ -73,11 +73,11 @@ public class SH1106SPI extends JWiringPiController {
     }
 
     public void showString(String string, int x, int y, int fontSize, boolean color) {
-        showString(string, x, y, fontSize, new Font("consolas", Font.PLAIN, fontSize), color);
+        showString(string, x, y, new Font("consolas", Font.PLAIN, fontSize), color);
     }
 
     public void showString(String string, int x, int y) {
-        showString(string, x, y, 12, new Font("consolas", Font.PLAIN, 12), true);
+        showString(string, x, y, new Font("consolas", Font.PLAIN, 12), true);
     }
 
     public void showMonoBitmap(int x, int y, short[] bmpBuffer,
